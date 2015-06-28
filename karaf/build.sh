d0eb53f4afec906daff5ebc2c89b50dc178b594c
Nicholas Hart <nic@lancs>
2015-06-28 17:59:42 +0100

    use karaf 3.0.2 not 3.0.3

--- karaf/build.sh
+++ karaf/build.sh
@@ -7,10 +7,10 @@ apt-get install oracle-java8-installer oracle-java8-set-default -y
 apt-get clean ; apt-get autoclean ; apt-get autoremove
 cd ; mkdir Downloads Applications
 cd Downloads
-wget http://download.nextag.com/apache/karaf/3.0.3/apache-karaf-3.0.3.tar.gz
+wget http://download.nextag.com/apache/karaf/3.0.2/apache-karaf-3.0.2.tar.gz
 wget http://archive.apache.org/dist/maven/maven-3/3.3.1/binaries/apache-maven-3.3.1-bin.tar.gz
-tar -zxvf apache-karaf-3.0.3.tar.gz -C /root/Applications/
+tar -zxvf apache-karaf-3.0.2.tar.gz -C /root/Applications/
 tar -zxvf apache-maven-3.3.1-bin.tar.gz -C /usr/local/
 echo 'PATH="/usr/local/apache-maven-3.3.1/bin:$PATH"' >> ~/.profile
-echo 'export KARAF_ROOT=/root/Applications/apache-karaf-3.0.3' >> ~/.profile
+echo 'export KARAF_ROOT=/root/Applications/apache-karaf-3.0.2' >> ~/.profile
 echo 'PATH="$KARAF_ROOT/bin:$PATH"' >> ~/.profile
