94a8036ab7f29a56c0d762c8062700ace5d2abd6
Nicholas Hart <nic@lancs>
2015-06-28 17:58:01 +0100

    new Dockers

---
+++ onosdeploy-1.1/package.sh
@@ -0,0 +1,6 @@
+source ~/.profile
+cd onos
+source  ~/onos/tools/dev/bash_profile
+export ONOS_USER=root ; export ONOS_GROUP=root ; \
+export OC1="10.30.65.204" ; export OCI="10.30.65.204" ; export ONOS_APPS="drivers,openflow,fwd,proxyarp,mobility" ; export ONOS_NIC="10.30.65.*" ; \
+onos-package
