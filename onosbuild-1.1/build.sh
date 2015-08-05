56deb6142dd8939a9c2448ba7408ca293e0228df
Nicholas Hart <nic@lancs>
2015-08-05 17:57:00 +0100

    fix karaf version mismatch

--- onosbuild-1.1/build.sh
+++ onosbuild-1.1/build.sh
@@ -2,6 +2,6 @@ source ~/.profile
 git clone https://gerrit.onosproject.org/onos -b onos-1.1
 cd onos
 source  ~/onos/tools/dev/bash_profile
-sed -i ~/Applications/apache-karaf-3.0.3/etc/org.apache.karaf.features.cfg -e '/featuresRepositories/s;$;,mvn:org.onosproject/onos-features/1.1.1-SNAPSHOT/xml/features;'
-patch --verbose -p1 < ~/diff2
+sed -i ~/Applications/apache-karaf-3.0.2/etc/org.apache.karaf.features.cfg -e '/featuresRepositories/s;$;,mvn:org.onosproject/onos-features/1.1.1-SNAPSHOT/xml/features;'
+patch --verbose -p1 < ~/diff3
 mvn clean install
